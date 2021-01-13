2678bfc167fcac4bf76f31a0ff2b1a5dadbdcbf6
Alexander Bonesteele <ajbonesteele@gmail.com>
2021-01-12 17:07:54 -0800

    first commit

---
+++ Assets/NetModular/Scripts/ChatManager.cs
@@ -0,0 +1,126 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using Photon.Chat;
+using Photon.Pun;
+using ExitGames.Client.Photon;
+
+public class ChatManager : MonoBehaviour, IChatClientListener
+{
+    ChatClient chatClient;
+
+    public InputField playerName;
+    public InputField chatInput;
+    public Text chatDisplay;
+
+    public GameObject joinChatPanel;
+    public GameObject worldChatPanel;
+
+    string worldChat;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        if (string.IsNullOrEmpty(PhotonNetwork.PhotonServerSettings.AppSettings.AppIdChat))
+        {
+            return;
+        }
+
+        worldChat = "world";
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (chatClient != null)
+        {
+            chatClient.Service();
+            if (Input.GetKey(KeyCode.Return))
+            {
+                SendMessage();
+            }
+        }
+    }
+
+    #region Public Methods
+
+    public void GetConnected()
+    {
+        chatClient = new ChatClient(this);
+        chatClient.Connect(PhotonNetwork.PhotonServerSettings.AppSettings.AppIdChat, PhotonNetwork.AppVersion, new AuthenticationValues(playerName.text));
+    }
+
+    public void SendMessage()
+    {
+        chatClient.PublishMessage(worldChat, chatInput.text);
+    }
+
+    #endregion
+
+    #region IChatClientListner implementation
+
+    public void DebugReturn(DebugLevel level, string message)
+    {
+        
+    }
+
+    public void OnChatStateChange(ChatState state)
+    {
+        
+    }
+
+    public void OnConnected()
+    {
+        joinChatPanel.SetActive(false);
+        worldChatPanel.SetActive(true);
+        chatClient.Subscribe(new string[] { worldChat });
+        chatClient.SetOnlineStatus(ChatUserStatus.Online);
+    }
+
+    public void OnDisconnected()
+    {
+        chatClient.SetOnlineStatus(ChatUserStatus.Offline);
+    }
+
+    public void OnGetMessages(string channelName, string[] senders, object[] messages)
+    {
+        for (int i = 0; i < senders.Length; i++)
+        {
+            chatDisplay.text += senders[i] + ": " + messages[i] + "\n";
+        }
+    }
+
+    public void OnPrivateMessage(string sender, object message, string channelName)
+    {
+        
+    }
+
+    public void OnStatusUpdate(string user, int status, bool gotMessage, object message)
+    {
+        
+    }
+
+    public void OnSubscribed(string[] channels, bool[] results)
+    {
+        foreach(var channel in channels)
+        {
+            chatClient.PublishMessage(channel, "joined");
+        }
+    }
+
+    public void OnUnsubscribed(string[] channels)
+    {
+        
+    }
+
+    public void OnUserSubscribed(string channel, string user)
+    {
+        
+    }
+
+    public void OnUserUnsubscribed(string channel, string user)
+    {
+        
+    }
+
+    #endregion
+}
---
+++ Assets/NetModular/Scripts/PlayerNameIputField.cs
@@ -0,0 +1,37 @@
+﻿using UnityEngine.UI;
+using UnityEngine;
+using Photon.Pun;
+using Photon.Realtime;
+
+[RequireComponent(typeof(InputField))]
+public class PlayerNameIputField : MonoBehaviour
+{
+    const string playerNamePrefKey = "PlayerName";
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        string defaultName = string.Empty;
+        InputField inputField = GetComponent<InputField>();
+        if (inputField != null)
+        {
+            if (PlayerPrefs.HasKey(playerNamePrefKey))
+            {
+                defaultName = PlayerPrefs.GetString(playerNamePrefKey);
+                inputField.text = defaultName;
+            }
+        }
+        PhotonNetwork.NickName = defaultName;
+    }
+
+    public void SetPlayerName(string value)
+    {
+        if (string.IsNullOrEmpty(value))
+        {
+            return;
+        }
+        PhotonNetwork.NickName = value;
+
+        PlayerPrefs.SetString(playerNamePrefKey, value);
+    }
+}
